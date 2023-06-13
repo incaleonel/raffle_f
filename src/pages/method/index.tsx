48486e61ac193c517a62addca25632b1fff50db8
Leonel Inca <inca.leonel@gmail.com>
2023-06-13 15:15:03 -0300

    styles modified

--- src/pages/method/index.tsx
+++ src/pages/method/index.tsx
@@ -1,9 +1,23 @@
 import { Button, Container } from '@mui/material'
+import { CSSProperties } from '@mui/material/styles/createMixins';
 import Image from 'next/image';
 import Link from 'next/link';
+
+
+const buttonStyles: CSSProperties = {
+  fontSize: '16px', // Cambia el tamaño de la fuente
+  backgroundColor: '#fff',
+  display:'flex',
+  flexDirection:'column',
+  width: '100%',
+  color: 'rgba(255, 155, 24, 0.999)',
+  fontWeight:'700', 
+  '&:hover': {
+    backgroundColor: 'transparent', // Quita el efecto hover
+  },
+};
 export default function Init() {
 
-  /* const dispatch = useAppDispatch(); */
   return (
 
     <Container maxWidth='md'>
@@ -11,13 +25,13 @@ export default function Init() {
       <h1 className='title tracking-in-expand-forward-top'>Elige un metodo de pago</h1>
       <div className='group-buttons'>
         <Link className='link' href="/method/mercadopago">
-          <Button className='buttonp slide-right' color='info' variant='contained'>
+          <Button style={buttonStyles} className='slide-right' variant='contained'>
             <Image  className='iconButton ' width={200} height={200} src='/mp.png' alt='icono de transferencia' />
             Mercadopago
           </Button>
         </Link>
         <Link className='link' href="/method/transference">
-          <Button className='buttonp slide-left' color='info'variant='contained' /* onClick={()=>dispatch(setTab('transference'))} */>
+          <Button style={buttonStyles} className='slide-left' variant='contained' >
             <div className='iconButton '>
 
               <Image className='transfer-icon' width={200} height={200} src='/transfer.png' alt='icono de transferencia'  />
